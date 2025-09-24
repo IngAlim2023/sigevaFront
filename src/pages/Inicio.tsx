cc31324ae8874d86e1fdc76b0050679c90347722
Macarron <macarront001@gmail.com>
2025-09-24 09:27:59 -0500

    landing

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -9,21 +9,21 @@ const Inicio: React.FC = () => {
         <Row>
           <Col xs={12} className="p-0">
             <img
-              src="/sena-image.jpg"
+              src="/coverphoto.png"
               alt="Imagen SENA"
               className="w-100 mb-4"
-              style={{ maxHeight: "230px", objectFit: "cover" }}
+              style={{ maxHeight: "230px", objectFit: "cover", borderRadius: "8px" }}
             />
           </Col>
         </Row>
 
-        <Row className="justify-content-center text-center">
-          <Col xs={12} md={10} lg={8}>
-            <h1 className="fw-bold display-5 mb-3">
+        <Row className="justify-content-center text-center" style={{ marginTop: "30px" }}>
+          <Col xs={12} md={10} lg={9}>
+            <h1 className="fw-bold display-5 fs-1 mb-3" >
               Participa, elige y haz parte del cambio
             </h1>
 
-            <p className="text-muted fs-5 mb-4">
+            <p className="text-muted fs-6 mb-4 max-w-600 mx-auto" style={{ maxWidth: "700px" }}>
               Una plataforma construida para que los aprendices del SENA puedan
               elegir a sus representantes.
             </p>
@@ -37,17 +37,18 @@ const Inicio: React.FC = () => {
                   backgroundColor: "#4CAF0C",
                   border: "none",
                   fontSize: "1.2rem",
+                  marginTop: "20px",
                 }}
               >
                 Ingresar
               </Button>
             </div>
 
-            <div className="d-flex flex-column flex-md-row align-items-center justify-content-center gap-3 mt-8">
-              <img src={Sigeva} alt="Logo SIGEVA" height={50} />
+            <div className="d-flex flex-column flex-md-row align-items-center justify-content-center gap-3 mt-8" style={{ marginTop: "60px" }}>
+              <img src={Sigeva} alt="Logo SIGEVA" height={40} />
               <div
                 className="d-none d-md-block"
-                style={{ width: 1, height: 40, backgroundColor: "gray" }}
+                style={{ width: 1, height: 30, backgroundColor: "gray" }}
               />
               <img
                 src="/logo_fabrica.png"
@@ -56,7 +57,7 @@ const Inicio: React.FC = () => {
               />
               <div
                 className="d-none d-md-block"
-                style={{ width: 1, height: 40, backgroundColor: "gray" }}
+                style={{ width: 1, height: 30, backgroundColor: "gray" }}
               />
               <Link
                 to="/equipo"
